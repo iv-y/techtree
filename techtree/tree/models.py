a45ea0e507ddaaf5ea44761f06c4b7f85325e8d0
iv-y <63594364+iv-y@users.noreply.github.com>
2020-06-01 18:24:56 +0000

    Add database models on tree.models, Do migrations

--- techtree/tree/models.py
+++ techtree/tree/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Department(models.Model):
+    full_name = models.CharField(max_length=50, unique=True)
+    short_name = models.CharField(max_length=10, unique=True)
+    alphabet_short_name = models.CharField(max_length=10, unique=True)
+    
+    def __str__(self):
+        return "학과: %s"%self.full_name
+    
+class CourseType(models.Model):
+    full_name = models.CharField(max_length=30, unique=True)
+    short_name = models.CharField(max_length=10, unique=True)
+    
+    def __str__(self):
+        return "과목 형태: %s"%self.full_name
+    
+class Course(models.Model):
+    full_name = models.CharField(max_length=100)
+    course_type = models.ForeignKey(CourseType, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    description = models.TextField(null=True, blank=True)
+    course_number = models.PositiveSmallIntegerField()
+    prerequisites = models.ManyToManyField("self", symmetrical=False, through="PrerequisiteData")
+    
+    def __str__(self):
+        return "과목: %s%d %s"%(self.department.alphabet_short_name, self.course_number, self.full_name)
+    
+    @property
+    def academic_year(self):
+        return self.course_number % 100
+    
+    @property
+    def course_code(self):
+        return "%s%d"%(self.department.alphabet_short_name, self.course_number)
+    
+class Alias(models.Model):
+    full_name = models.CharField(max_length=50)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    liked_users = models.ManyToManyField(User)
+    
+    def __str__(self):
+        return "별칭: %s %s"%(self.full_name, self.course.course_code)
+    
+class PrerequisiteData(models.Model):
+    parent_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="prerequisite_data_as_parent")
+    child_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="prerequisite_data_as_child")
+    liked_users = models.ManyToManyField(User)
+    
