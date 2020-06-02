e38851e2304ffc7257b56863336df1ffa6291139
iv-y <63594364+iv-y@users.noreply.github.com>
2020-06-02 03:27:18 +0000

    Change tree.models for better data processing

--- techtree/tree/models.py
+++ techtree/tree/models.py
@@ -23,7 +23,7 @@ class Course(models.Model):
     course_type = models.ForeignKey(CourseType, on_delete=models.PROTECT)
     department = models.ForeignKey(Department, on_delete=models.PROTECT)
     description = models.TextField(null=True, blank=True)
-    course_number = models.PositiveSmallIntegerField()
+    course_code = models.CharField(max_length=20, unique=True)
     prerequisites = models.ManyToManyField("self", symmetrical=False, through="PrerequisiteData")
     
     def __str__(self):
@@ -34,27 +34,36 @@ class Course(models.Model):
         return self.course_number // 100
     
     @property
-    def course_code(self):
-        return "%s%d"%(self.department.alphabet_short_name, self.course_number)
+    def course_number(self):
+        return int("".join(filter(str.isdigit, self.course_code)))
     
 class Alias(models.Model):
     full_name = models.CharField(max_length=50)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="written_aliases")
-    liked_users = models.ManyToManyField(User, related_name="liked_aliases")
+    liked_users = models.ManyToManyField(User, related_name="liked_aliases", blank=True)
     
     def __str__(self):
-        return "별칭: %s %s"%(self.full_name, self.course.course_code)
+        return "별칭: %s %s ++%d"%(self.full_name, self.course.course_code, self.liked_users.count())
     
     class Meta:
         verbose_name_plural="Aliases"
+        constraints=[
+            models.UniqueConstraint(fields=['full_name', 'course'], name='unique_alias_for_course'),
+        ]
     
 class PrerequisiteData(models.Model):
     parent_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="prerequisite_dataset_as_parent")
     child_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="prerequisite_dataset_as_child")
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="written_prerequisites")
-    liked_users = models.ManyToManyField(User, related_name="liked_prerequisites")
+    liked_users = models.ManyToManyField(User, related_name="liked_prerequisites", blank=True)
+    
+    def __str__(self):
+        return "선수과목 데이터: %s >>> %s ++%d"%(self.parent_course.course_code, self.child_course.course_code, self.liked_users.count())
     
     class Meta:
         verbose_name_plural="Prerequisite Dataset"
+        constraints=[
+            models.UniqueConstraint(fields=['parent_course', 'child_course'], name='unique_prerequisite_data'),
+        ]
     
